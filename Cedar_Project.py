643b67cde9e80e44a8f4da6bb7f72e5085f9b710
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-09-08 22:52:57 -0700

    learned formatting

--- Cedar_Project.py
+++ Cedar_Project.py
@@ -18,7 +18,11 @@ print ("My number is",var)
 name = input("Enter your name: ")
 print (f"Hello {name}")
 
-num1 = int(input("Enter firt number: "))
+num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 print (num1 + num2)
 
+#Formatting numbers
+print ("My number is {0}. Your number is {1}.".format(num1,num2))
+#Alt method:
+print (f"My number is {num1}. Your number is {num2}.")
