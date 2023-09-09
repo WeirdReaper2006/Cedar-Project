5b380c944de4a0e86b3bccfd5bb003d6b44aca71
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-09-08 22:43:15 -0700

    hello wrld, variable, input name,input and add two nums

--- Cedar_Project.py
+++ Cedar_Project.py
@@ -1,1 +1,23 @@
 print ("hello, World")
+
+var = 10
+print (var)
+var = var + 10
+print(var+5)
+print(var-5)
+print(var/5)
+print(var*5)
+print(var**5)
+print(var//5)
+print(var%5)
+
+print ("My number is",var)
+#Alternate method is:
+#print (f"My number is {var}")
+
+name = input("Enter your name:")
+print (f"Hello {name}")
+
+num1 = int(input("Enter firt number"))
+num2 = int(input("Enter second number"))
+print (num1 + num2)
