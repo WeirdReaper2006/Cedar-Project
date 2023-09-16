ae77b91fbd739bb08b1c6b68376c9d2c9b60a475
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-09-15 22:47:17 -0700

    CASE statements (Nested IF satements) FOR Loop

--- 16_Sept_23.py
+++ 16_Sept_23.py
@@ -1,1 +1,29 @@
+#Case statements(there are no case statements so we used nested if statements)
+Grade = input ("Enter your grade: ")
 
+if Grade == "A":
+    print ("Top Grade")
+elif Grade =="F" or Grade =="U":
+    print ("Fail")
+elif Grade in ("B", "C", "D", "E"):
+    print ("Pass")
+else:
+    print ("Invalid Grade")
+    
+#For Loops (count-controlled loop)
+for x in range (5):
+    print (x, end = " ")
+    
+print ()
+for x in range (2, 14, 3):
+    print (x, end = " ")
+    
+print ()
+for x in range (5, 1, -1):
+    print (x, end = " ")
+    
+print ()
+for x in ("a", "b", "c"):
+    print (x, end = " ")
+    
+print ()
