2be3985c0310c839591145dc2de4635d8187bcfb
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-09-18 07:00:26 -0700

    uncommented lines

--- 16_Sept_23.py
+++ 16_Sept_23.py
@@ -1,32 +1,32 @@
-# #Case statements(there are no case statements so we used nested if statements)
-# Grade = input ("Enter your grade: ")
+#Case statements(there are no case statements so we used nested if statements)
+Grade = input ("Enter your grade: ")
 
-# if Grade == "A":
-#     print ("Top Grade")
-# elif Grade =="F" or Grade =="U":
-#     print ("Fail")
-# elif Grade in ("B", "C", "D", "E"):
-#     print ("Pass")
-# else:
-#     print ("Invalid Grade")
+if Grade == "A":
+    print ("Top Grade")
+elif Grade =="F" or Grade =="U":
+    print ("Fail")
+elif Grade in ("B", "C", "D", "E"):
+    print ("Pass")
+else:
+    print ("Invalid Grade")
     
-# #For Loops (count-controlled loop)
-# for x in range (5):
-#     print (x, end = " ")
+#For Loops (count-controlled loop)
+for x in range (5):
+    print (x, end = " ")
     
-# print ()
-# for x in range (2, 14, 3):
-#     print (x, end = " ")
+print ()
+for x in range (2, 14, 3):
+    print (x, end = " ")
     
-# print ()
-# for x in range (5, 1, -1):
-#     print (x, end = " ")
+print ()
+for x in range (5, 1, -1):
+    print (x, end = " ")
     
-# print ()
-# for x in ("a", "b", "c"):
-#     print (x, end = " ")
+print ()
+for x in ("a", "b", "c"):
+    print (x, end = " ")
     
-# print ()
+print ()
 
 
 #Q1) Ask a user for a number and print its multiplication table (1-10)
