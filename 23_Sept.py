3a530ee7e43e0eb150e3aa58b5ab0138daeef1b5
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-09-22 22:31:32 -0700

    wrote questions

--- 23_Sept.py
+++ 23_Sept.py
@@ -1,1 +1,4 @@
-
+# Q1. convert the triangle code to only use while loop
+# Q2. convert the pyramid code to only use while loop 
+# Q3. create a two player rock , paper , scissor , game which vaidates the user innput
+# ask user for the number of rounds, output winner of each round and overall game
