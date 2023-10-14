c9d5e5565acf26110fe6059a64e30a193b2a2fde
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-10-13 22:47:08 -0700

    String Manipulation

--- OCTOBER/14_Oct.py
+++ OCTOBER/14_Oct.py
@@ -1,1 +1,19 @@
-
+myStr = "This is Cedar College"
+print(myStr[5])
+print(chr(65))
+print(ord("a"))
+print(len(myStr))
+#start:end]
+#left empty: start = 0
+#right empty: end = length
+#left nog: start - longth - value
+#right nog: end = length - value
+#MID
+print(myStr[2:6])
+print(myStr[0:5])
+#LEFT
+print(myStr[:5])
+print(myStr[:-7])
+#RIGHT
+print(myStr[14:])
+print(myStr[-7:])
