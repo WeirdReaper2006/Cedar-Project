6f98e82d6673de185a98c9fdd0ea0241c55dde7d
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-10-13 23:25:10 -0700

    task 1 and 2

--- OCTOBER/14_Oct.py
+++ OCTOBER/14_Oct.py
@@ -1,3 +1,7 @@
+from pickle import FALSE
+from tkinter import TRUE
+
+
 myStr = "This is Cedar College"
 print(myStr[5])
 print(chr(65))
@@ -17,3 +21,27 @@ print(myStr[:-7])
 #RIGHT
 print(myStr[14:])
 print(myStr[-7:])
+
+print(myStr.upper())
+print(myStr.lower())
+myNum = 7.99
+print(int(myNum))
+strNum = "7"
+print(int(strNum))
+strNum2 = "7.45"
+print(float(strNum2))
+
+#Task 1: Ask user for a string and output if string is a palindrome
+string = input("Enter a word: ")
+is_palindrome = True
+for x in range(len(string) // 2):
+    if string[x] != string[-x - 1]:
+        is_palindrome = False
+if is_palindrome:
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
+#Task 2: Ask user for a string, convert and store it in reverse
+user_input = input("Enter a string: ")
+reversed_string = user_input[::-1]
+print("Reversed string:", reversed_string)
