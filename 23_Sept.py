59ad09421cd1b01f53fc318415ce3cef39bcda22
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-10-13 22:20:32 -0700

    final

--- 23_Sept.py
+++ 23_Sept.py
@@ -26,3 +26,17 @@ while line in range(0, num + 1):
     line +=1
 # Q3. create a two player rock , paper , scissor , game which vaidates the user innput
 # ask user for the number of rounds, output winner of each round and overall game
+rounds = int(input("Enter number of rounds: "))
+p1Score = 0
+p2Score = 0
+for i in range (rounds):
+    p1Input = input("Player 1, Enter r, s or p: ")
+    while p1Input != "r" and p1Input != "s" and p1Input != "p":
+        print ()
+        
+#Q4, ask user to think of a number between 1 and 100
+#your program can ask yes or no questions (max 10)
+#your program should tell the correct guess everytime
+print ("Please think of a number between 1 and 100: ")
+Q1 = input("Is your number even. Enter y or n: ")
+
