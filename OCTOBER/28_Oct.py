17bea1864982d75c8ce86064da6e1e83a9486e35
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-10-28 10:30:08 +0500

    made two functions

--- OCTOBER/28_Oct.py
+++ OCTOBER/28_Oct.py
@@ -1,1 +1,18 @@
+#PROCEDURE = VOID Function
+#FUNCTION = Fruitful functions
+def inputOddNumber():
+    number = 0
+    while number % 2 == 0:
+        number = int(input("Enter odd number: "))
+    return number
+#Main program starts here
+newNumber = inputOddNumber()
+print ("Number entered =",newNumber)
 
+def OutputSmbol(numberofsymbols, symbol):
+    for count in range(numberofsymbols):
+        print (symbol, end="")
+    print()
+    
+#main program starts here
+OutputSmbol(10, "$")
