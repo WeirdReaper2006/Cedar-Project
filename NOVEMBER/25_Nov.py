17a0a2673aa35f7ded0edf6d9491e2e39ce12cca
WeirdReaper2006 <rayyanrafique95@gmail.com>
2023-11-25 10:51:30 +0500

    recursive functions, Task 1 and 2

--- NOVEMBER/25_Nov.py
+++ NOVEMBER/25_Nov.py
@@ -1,1 +1,19 @@
-
+def recursiveFunction (num1):
+    print (num1)
+    if num1 < 5:
+        recursiveFunction (num1+1)
+    
+#Task1: Make a recursive function that counts down from the parameter given till 1
+def countDown (num2):
+    print (num2)
+    if num2 > 1:
+        countDown (num2 - 1)
+        
+#Task2: Make a function that returns the sum of all value from parameter number
+def FindSumFrom1 (num3):
+    if num3 > 1:
+        return num3 + FindSumFrom1 (num3 - 1)
+    else:
+        return 1
+        
+print (FindSumFrom1 (15))
